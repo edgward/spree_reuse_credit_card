5f42b61fde418818c014f4bb4f98de3b4af7b411
Ed Chang <t.edward.chang@gmail.com>
2014-10-30 02:24:53 -0400

    resolving deface warning

--- app/overrides/add_credit_card_list_to_user_account_form.html.rb
+++ app/overrides/add_credit_card_list_to_user_account_form.html.rb
@@ -3,4 +3,5 @@ Deface::Override.new(
                      :virtual_path => 'spree/users/show',
                      :insert_after => '[data-hook=account_my_orders]',
                      :partial =>'spree/users/card_admin'
+										 :original => 'd2bf74b421eb9949acbb83227f89b1e895cebce2'
 )
